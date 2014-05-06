084c4f83c77b2bac44506b8b2bf2e4e8f7ebad73
Glenn <gleenn@gmail.com>
2014-05-06 16:11:38 +0900

    all evaluator code works

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -20,20 +20,55 @@ def evaluate(ast, env):
     if is_atom(ast):
         return ast
 
-    if ast[0] == "quote":
+    function_name = ast[0]
+
+    if function_name == "quote":
         if len(ast) == 2:
             return ast[1]
         else:
             return []
 
-    if ast[0] == "atom":
-        return is_atom(evaluate(ast[1], env))
+    first_arg = evaluate(ast[1], env)
 
-    if ast[0] == "eq":
+    if function_name == "atom":
+        return is_atom(first_arg)
+
+    second_arg = evaluate(ast[2], env)
+
+    if function_name == "eq":
         evaluated_items = [evaluate(item, env) for item in ast[1:]]
         for i in range(len(evaluated_items) - 1):
             return is_atom(evaluated_items[i]) and evaluated_items[i] == evaluated_items[i + 1]
         else:
             return True
+
+    print function_name
+    print first_arg
+    print second_arg
+    if function_name in ["+", "-", "*", "/", "mod", "<", ">"] and not (is_integer(first_arg) and is_integer(second_arg)):
+        error_message = "Math functions only take integer args but you tried to do (%s, %s, %s)" % (
+        function_name, first_arg, second_arg)
+        raise LispError(error_message)
+
+    if function_name == "+":
+        return first_arg + second_arg
+
+    if function_name == "-":
+        return first_arg - second_arg
+
+    if function_name == "*":
+        return first_arg * second_arg
+
+    if function_name == "/":
+        return first_arg / second_arg
+
+    if function_name == "mod":
+        return first_arg % second_arg
+
+    if function_name == ">":
+        return first_arg > second_arg
+
+    if function_name == "<":
+        return first_arg < second_arg
 
     return [evaluate(x, env) for x in ast]
